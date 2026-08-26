5c5b964fa71f2da2c5a4499d227d33e885cf370a
Danish Ali <rajpootdanishali3@gmail.com>
2026-08-26 19:55:26 +0500

    Update minSdkVersion in build.gradle.kts to use flutter configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
     defaultConfig {
         applicationId = "com.example.doc_vault"
 
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
 
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
